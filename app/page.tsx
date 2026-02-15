27c35e132987e06e3611a50f714e42f6ef53bcb7
sh3ki <shekaigarcia@gmail.com>
2026-02-16 05:48:42 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import Chatbot from '@/components/Chatbot';
 
 export default function Home() {
   return (
-    <div className="bg-dark">
+    <div>
       <Header />
       <main>
         <Hero />
